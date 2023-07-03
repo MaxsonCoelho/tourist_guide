9223da2331b62424adadebcfb35082daca5b3182
Maxson Coelho <maxsoncoelho@gmail.com>
2023-07-03 15:03:24 -0400

    get data

--- lib/src/pages/preload.dart
+++ lib/src/pages/preload.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import '../models/appdata.dart';
 class PreloadPage extends StatefulWidget {
   const PreloadPage({super.key});
 
@@ -10,8 +12,45 @@ class PreloadPage extends StatefulWidget {
 class _PreloadPage extends State<PreloadPage> {
   bool loading = true;
 
+  void requestInfo() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      loading = true;
+    });
+
+    //chamar um método asyncrono (http) de outra classe widget
+    // ignore: use_build_context_synchronously
+    bool req = await Provider.of<AppData>(context, listen: false).requestData();
+
+    if(req) {
+      //navega e esconde o botão voltar
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, '/home');
+      //navega sem esconder o botão voltar
+      // Navigator.pushNamed(context, '/home');
+    }else {
+      setState(() {
+      loading = false;
+    });
+    }
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    requestInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return Scaffold(
       body: Center(
         child: Column(
